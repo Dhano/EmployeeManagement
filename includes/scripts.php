6f1b22bd56cecc9d97babfbaf29bad9a5869f930
Nikhil Shadija <user@NIKS.Niks>
2018-03-30 16:44:09 +0530

    Uploading Template

---
+++ includes/scripts.php
@@ -0,0 +1,21 @@
+<!--   Core JS Files   -->
+<script src="assets/vendors/jquery1.10.2/jquery-1.10.2.js" type="text/javascript"></script>
+<script src="assets/vendors/bootstrap3/bootstrap.min.js" type="text/javascript"></script>
+
+<!--  Checkbox, Radio & Switch Plugins -->
+<script src="assets/vendors/bootstrap3/bootstrap-checkbox-radio.js"></script>
+
+<!--  Charts Plugin -->
+<script src="assets/vendors/chartlistjs/chartist.min.js"></script>
+
+<!--  Notifications Plugin    -->
+<script src="assets/vendors/bootstrap3/bootstrap-notify.js"></script>
+
+<!--  Google Maps Plugin    -->
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
+
+<!-- Paper Dashboard Core javascript and methods for Demo purpose -->
+<script src="assets/js/paper-dashboard.js"></script>
+
+<!-- Custom JS -->
+<script src="assets/js/script.js"></script>
